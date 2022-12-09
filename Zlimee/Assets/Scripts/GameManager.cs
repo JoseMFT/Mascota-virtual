fd7c095575ccb5439274fb0169c257e3dcdf6e9c
JoseMFT <jomaft03@gmail.com>
2022-12-09 12:49:24 +0000

    atomarporculoqmejodia

--- Zlimee/Assets/Scripts/GameManager.cs
+++ Zlimee/Assets/Scripts/GameManager.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+//using UnityEngine.SceneManagement;
 
-public class gameManager: MonoBehaviour {
-    public void ClickedPlay () {
+public class GameManager: MonoBehaviour {
+    // Start is called before the first frame update
+    /*public void ClickedPlay () {
         SceneManager.LoadScene (2);
     }
     public void ClickedFeed () {
         SceneManager.LoadScene (1);
-    }
+    }*/
 }
